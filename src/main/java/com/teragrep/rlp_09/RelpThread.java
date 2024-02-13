e3190971bf61ca58722187d9a4a4cc5cad863c0e
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2024-02-13 13:54:28 +0200

    Minor cleanup

--- src/main/java/com/teragrep/rlp_09/RelpThread.java
+++ src/main/java/com/teragrep/rlp_09/RelpThread.java
@@ -14,24 +14,19 @@ class RelpThread implements Runnable {
     private final int port;
     private final byte[] message;
     private final AtomicLong messagesSent;
+    private final RelpConnection relpConnection;
     RelpThread(String name, String target, int port, byte[] message, AtomicLong messagesSent) {
         this.name = name;
         this.target = target;
         this.port = port;
         this.message = message;
         this.messagesSent = messagesSent;
+        this.relpConnection = new RelpConnection();
     }
     @Override
     public void run() {
         System.out.printf("[%s] Starting%n", name);
-        RelpConnection relpConnection = new RelpConnection();
-        try {
-            relpConnection.connect(target, port);
-        } catch (IOException | TimeoutException e) {
-            System.out.println("Failed to connect to the server");
-            System.exit(1);
-            throw new RuntimeException(e);
-        }
+        connect();
         while(stayRunning) {
             RelpBatch relpBatch = new RelpBatch();
             relpBatch.insert(message);
@@ -48,18 +43,21 @@ class RelpThread implements Runnable {
                     System.out.printf("[%s] Failed to send message%n", name);
                     relpBatch.retryAllFailed();
                     relpConnection.tearDown();
-                    try {
-                        relpConnection.connect(target, port);
-                    } catch (IOException | TimeoutException e) {
-                        System.out.printf("[%s] Failed to connect to the server%n", name);
-                        System.exit(1);
-                    }
                 } else {
                     notSent = false;
                 }
             }
         }
         System.out.printf("[%s] Exiting%n", name);
+    }
+
+    public void connect() {
+        try {
+            relpConnection.connect(target, port);
+        } catch (IOException | TimeoutException e) {
+            System.out.printf("[%s] Failed to connect to the server%n", name);
+            System.exit(1);
+        }
     }
     public void shutdown() {
         stayRunning = false;
